7c6e2311006c5431dd5b1d2b0ffa2d1a7fbe5b3c
Fabio Urquiza <flus@cesar.org.br>
2020-01-28 13:29:03 -0300

    Add restrictions to login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,5 @@
 class SessionsController < ApplicationController
+  before_action :logged_in_redirect, except: [:destroy]
 
   def new
   end
@@ -10,7 +11,7 @@ class SessionsController < ApplicationController
       flash[:success] = "You have successfully logged in"
       redirect_to root_path
     else
-      flash.now[:success] = "There was something wrong with your login information"
+      flash.now[:error] = "There was something wrong with your login information"
       render :new
     end
   end
@@ -19,5 +20,14 @@ class SessionsController < ApplicationController
     session[:user_id] = nil
     flash[:success] = "You have successfully logged out"
     redirect_to login_path
+  end
+
+  private
+
+  def logged_in_redirect
+    if logged_in?
+      flash[:error] = "You are already logged in"
+      redirect_to root_path
+    end
   end
 end
